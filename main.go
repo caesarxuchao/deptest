bdbcfac3d04473b64cf600390392986b83d9449e
Chao Xu <xuchao@google.com>
2017-02-21 11:22:02 -0800

    1

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	depa "github.com/caesarxuchao/depa/main"
+	depb "github.com/caesarxuchao/depb/main"
+)
+
+func main() {
+	fmt.Println("vim-go")
+}
+
+var x depa.A
+var y depb.B
